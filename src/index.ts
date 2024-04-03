2e1ad439d1809534acef4f14acdfa635f0061346
Abdelrahman Khalil <samyz.k71@gmail.com>
2024-04-03 15:20:55 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "./calculator";
 export * from "./methods";
 export * from "./types";
 export * from './dates'
-export { format } from "./format";
+export { format, formatTime } from "./format";
 export { tune } from "./tune";
